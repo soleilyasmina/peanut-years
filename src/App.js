d61e29aa0319ddada026d13e9d68d4419148b2fd
soleilyasmina <mounlasolomon@gmail.com>
2020-08-21 16:01:47 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import tweet from './images/IMG_0400.png';
 import './App.css';
 
 function App() {
+  const [birthdate, setBirthdate] = useState('');
+  const [peanutAge, setPeanutAge] = useState(null);
+
+  const handleSubmit = (e) => {
+    const today = new Date();
+    const birth = new Date(birthdate);
+    const UNIXDifference = today.getTime() - birth.getTime();
+    const years = UNIXDifference / (1000 * 60 * 60 * 24 * 365.25);
+    setPeanutAge(years.toFixed(2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="App">
+        <img src={tweet} alt="tweet"/>
+        <h4>How old are you in Peanut Years?</h4>
+        <input onChange={(e) => setBirthdate(e.target.value)} type="date" max={new Date().toISOString().split('T')[0]}/>
+        <button onClick={handleSubmit}>Calculate!</button>
+        {peanutAge && <h5>You are {peanutAge} peanut years old.</h5>}
+      </div>
+      <footer>made by&nbsp;
+        <a href="https://soleilyasmina.com">
+          @soleilyasmina
         </a>
-      </header>
-    </div>
+      </footer>
+    </>
   );
 }
 
